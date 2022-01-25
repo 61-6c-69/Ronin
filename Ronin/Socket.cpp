998ad9d2263f2c47d9c37025ed2745919e4a0791
ali <alifarshad13@gmail.com>
2022-01-25 13:14:14 +0330

    add socket methods

--- Ronin/Socket.cpp
+++ Ronin/Socket.cpp
@@ -25,3 +25,44 @@ bool Socket::Connect(){
 	return true;
 }
 
+
+void Socket::Recv(void(*callback)(char*)){
+	char* msg = new char[SERVER_RECV_BUFF_SIZE];
+	int msg_size;
+
+	while (true){
+		//recv
+		msg_size = recv(this->sock, msg, SERVER_RECV_BUFF_SIZE, 0);
+		if (msg_size == SOCKET_ERROR){
+			cout << "recv err"<<GetLastError();
+			return;
+		}
+
+		msg[msg_size] = '\0';
+		callback(msg);
+	}
+}
+
+bool Socket::Send(char* data){
+	int s = send(this->sock, data, strlen(data), 0);
+	if (s == SOCKET_ERROR){
+		return false;
+	}
+
+	return true;
+}
+
+void Socket::Close(){
+	closesocket(this->sock);
+	WSACleanup();
+}
+
+bool Socket::isConnect(){
+	int e;
+	int esize = sizeof(int);
+	int er = getsockopt(this->sock, SOL_SOCKET, SO_ERROR, (char*)&e, &esize);
+	if (er == SOCKET_ERROR){
+		return false;
+	}
+	return true;
+}
--- Ronin/Socket.h
+++ Ronin/Socket.h
@@ -6,14 +6,15 @@ private:
 	const int address_family = AF_INET;
 	const int type = SOCK_STREAM;
 	const int protocol = IPPROTO_TCP;
+
 public:
 	int port;
 	char* host;
 	SOCKET sock;
 
 	bool Connect();
-	void Recv(void(char* data));
-	void Send(char* data);
-	bool Close();
+	void Recv(void(*callback)(char*));
+	bool Send(char* data);
+	void Close();
 	bool isConnect();
 };
