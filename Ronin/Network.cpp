46d0ec01509bad0749b96a6db067df723a389157
ali <alifarshad13@gmail.com>
2022-02-04 15:27:19 +0330

    add network

---
+++ Ronin/Network.cpp
@@ -0,0 +1,32 @@
+#include"Network.h"
+
+bool Network::Connect(){
+	if (this->socket.Connect()){
+		return true;
+	}
+	return false;
+}
+
+void Network::DisConnect(){
+	if (this->socket.isConnect()){
+		this->socket.Close();
+	}
+}
+
+void ListenSocket(char* data){
+	try{
+		json::JSON obj = json::JSON::Load(data);
+		CommandMap::Init().Call(obj["command"].ToString());
+	}
+	catch (exception e){
+		cout << "json parse error";
+	}
+}
+
+bool Network::Listen(){
+	if (this->socket.isConnect()){
+		this->socket.Recv(ListenSocket);
+		return true;
+	}
+	return false;
+}
---
+++ Ronin/Network.h
@@ -0,0 +1,19 @@
+#pragma once
+#include"Socket.h"
+#include"json.h"
+#include"CommandMap.h"
+
+class Network{
+private:
+	Socket socket;
+
+public:
+	static Network& Init(){
+		static Network instance;
+		return instance;
+	}
+
+	bool Connect();
+	void DisConnect();
+	bool Listen();
+};
