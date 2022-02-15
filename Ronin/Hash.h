6814394857c13d625ae03067df1281ae580064b1
ali <alifarshad13@gmail.com>
2022-02-15 12:28:32 +0330

    update hash class

--- Ronin/Hash.h
+++ Ronin/Hash.h
@@ -1,32 +1,37 @@
 #pragma once
 #include"Crypto.h"
 class Hash{
+private:
+	Crypto crypto;
+	char* str;
+
+	void setHashInfo(int size, DWORD calg, DWORD prov = PROV_RSA_FULL){
+		this->crypto.buf_size = size;
+		this->crypto.hash_calg = calg;
+		this->crypto.hash_prov = prov;
+	}
+
 public:
-	//md5
-	static char* md5(char* str){
-		Crypto ca;
-		ca.buf_size = 16;
-		ca.hash_calg = CALG_MD5;
-		ca.hash_prov = PROV_RSA_FULL;
-		return ca.Create(str);
+	Hash(char* str){
+		this->str = str;
 	}
 
-	//sha1
-	static char* sha1(char* str){
-		Crypto ca;
-		ca.buf_size = 20;
-		ca.hash_calg = CALG_SHA1;
-		ca.hash_prov = PROV_RSA_FULL;
-		return ca.Create(str);
+	Hash& md5(){
+		this->setHashInfo(16, CALG_MD5);
+		return *this;
 	}
 
-	//sha256
-	static char* sha256(char* str){
-		Crypto ca;
-		ca.buf_size = 32;
-		ca.hash_calg = CALG_SHA_256;
-		ca.hash_prov = PROV_RSA_FULL;
-		return ca.Create(str);
+	Hash& sha1(){
+		this->setHashInfo(20, CALG_SHA1);
+		return *this;
 	}
 
+	Hash& sha256(){
+		this->setHashInfo(32, CALG_SHA_256);
+		return *this;
+	}
+
+	char* get(){
+		return crypto.Create(this->str);
+	}
 };
