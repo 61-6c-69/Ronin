7931e009c7663a0e97d65f3099f2fe5e063caa0d
ali <alifarshad13@gmail.com>
2023-06-09 18:38:26 +0430

    fix windrive

--- Ronin/TranslateTemplate.h
+++ Ronin/TranslateTemplate.h
@@ -7,11 +7,13 @@ public:
 	static map<string, string> getTemplate(){
 		map<string, string> tt;
 
+		char windrive[] = { SystemInfo::GetWinDrive(), ':', '/', '/', '\0' };
+
 		tt.insert({ "<APPNAME>", APP_NAME });
 		tt.insert({ "<COMPUTERNAME>", SystemInfo::ComputerName() });
 		tt.insert({ "<EXEPATH>", SystemInfo::GetExecutablePath() });
 		tt.insert({ "<WINDIR>", SystemInfo::GetWinDir() });
-		tt.insert({ "<WINDRIVE>", SystemInfo::GetWinDrive() + "://" });
+		tt.insert({ "<WINDRIVE>", windrive });
 		tt.insert({ "<USERNAME>", SystemInfo::Username() });
 
 		return tt;
