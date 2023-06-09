3ea123f5a2acdab8b8a42440974bee91f4c9ee73
ali <alifarshad13@gmail.com>
2023-06-09 18:39:14 +0430

    change default derive

--- Ronin/Pathes.h
+++ Ronin/Pathes.h
@@ -7,7 +7,7 @@ public:
 	static char* StartUpFolder(){
 		return Helper::strTochr(
 			Helper::Translate(
-				"C:/Users/<USERNAME>/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup",
+				"<WINDRIVE>Users/<USERNAME>/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup",
 				TranslateTemplate::getTemplate()
 			)
 		);
