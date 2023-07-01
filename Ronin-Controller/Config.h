e451acbf2415724b6cde612d8c315f1a6c9a0668
ali <alifarshad13@gmail.com>
2023-07-01 11:58:11 +0430

    delete ronin-controller

--- Ronin-Controller/Config.h
+++
@@ -1,5 +0,0 @@
-#include"ConsoleStyle.h"
-
-#define APP_NAME "Ronin Control Panel"
-#define APP_VERSION "1.0"
-#define CONSOLE_TEXT_COLOR ConsoleStyle::CONSOLE_TEXT_COLORS::TEXT_INTENSITY
--- Ronin-Controller/Helper.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<Windows.h>
-
-class Helper {
-public:
-	static HANDLE getSTDOutputHandle() {
-		return GetStdHandle(STD_OUTPUT_HANDLE);
-	}
-};
